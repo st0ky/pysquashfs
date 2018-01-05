10ffc1e417967f319bf44c6f0295a6ef4c1ede9d
Your Name <you@example.com>
2018-01-05 11:55:23 +0200

    more one commit

--- src/mksquashfs.py
+++ src/mksquashfs.py
@@ -2,11 +2,11 @@ import cstruct2py
 import cstruct2py.c2py as types
 import os
 
-squashfs_tools_src = os.path.join(os.path.dirname(os.path.abspath(__file__)), "/../squashfs-tools/")
+squashfs_tools_src = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", "squashfs-tools")
 
 import IPython
-IPython.Embed()
-types.parse_file("./includes.h", )
+# IPython.embed()
+types.parse_file("./includes.h", [squashfs_tools_src, r"C:\binyam\squashfs\src\cstruct2py\pycparser\utils\fake_libc_include"], save_tmp=True, debuglevel=0)
 class SquashFS(object):
 	def __init__(self, arg):
 		super(SquashFS, self).__init__()
